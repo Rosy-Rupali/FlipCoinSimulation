5f235d89c459ef0441ee1dd09ace562c4532eb0f
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-05-26 10:11:26 +0530

    wining percentage of head and tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,34 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 echo "This problem displays winning percentage of Head and Tail combination in a Singlet, Doublet and Triplet"
 
-flipCoin=$((RANDOM%2))
-if [ $flipCoin -eq 0 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+read -p "number of times user want to flip the coin" flip
+
+countHead=0
+countTail=0
+
+declare -A noOfCounts
+
+for((i=0; i<$flip; i++))
+do
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		((countHead++))
+	else
+		((countTail++))
+	fi
+done
+
+echo "head="$countHead
+echo "tail="$countTail
+
+noOfCounts[tail]=$countTail
+noOfCounts[head]=$countHead
+
+echo ${noOfCounts[@]}
+
+PercentageOfTails=$((100*$countTail/$flip))
+echo "Percentage of tails= "$PercentageOfTails
+PercentageOfHeads=$((100*$countHead/$flip))
+echo "Percentage of heads= "$PercentageOfHeads
+
