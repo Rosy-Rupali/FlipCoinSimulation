34677ce6d9a9833af4b0a20e73552b82af972f9c
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-05-26 09:30:00 +0530

    checking flip coin head and tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,3 +2,10 @@
 
 echo "This problem displays winning percentage of Head and Tail combination in a Singlet, Doublet and Triplet"
 
+flipCoin=$((RANDOM%2))
+if [ $flipCoin -eq 0 ]
+then
+	echo "Heads"
+else
+	echo "Tails"
+fi
